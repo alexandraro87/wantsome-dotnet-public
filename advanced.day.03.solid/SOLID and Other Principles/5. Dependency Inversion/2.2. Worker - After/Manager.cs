4fec39b80a351c6a7ec396c6bc6e1bef6085d48d
Alexandra Dospinescu <alexandra.carauta@webbeds.com>
2019-12-23 16:37:30 +0200

    5 After

---
+++ advanced.day.03.solid/SOLID and Other Principles/5. Dependency Inversion/2.2. Worker - After/Manager.cs
@@ -0,0 +1,17 @@
+﻿namespace DependencyInversionWorkerAfter
+{
+    public class Manager
+    {
+        private readonly Worker worker;
+
+        public Manager(Worker worker)
+        {
+            this.worker = worker;
+        }
+
+        public void Manage()
+        {
+            this.worker.Work();
+        }
+    }
+}
